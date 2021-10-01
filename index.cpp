2c8737c3c7762c5accc73256fa91da5830ab6fc4
Kapil Khandelwal <42177808+thekapilkhandelwal@users.noreply.github.com>
2021-10-01 22:22:34 +0530

    created welcome code
    added a welcome message with name.

---
+++ index.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string message = "Welcome to Hacktoberfest 2021";
+  string name;
+  cout<<"Enter your name ";
+  cin>>name;
+  cout<<"\n"<<message + " " + name+"!"<<endl;
+  return 0;
+}
